f599fb94e753b8f97587a54c3d3c9251540171c7
yktaykketo <102287488+yktaykketo@users.noreply.github.com>
2023-09-25 17:26:42 -0400

    Update quadratic.py

--- ps-2/quadratic.py
+++ ps-2/quadratic.py
@@ -10,13 +10,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.rcParams['text.usetex'] = True
-def quadratic():
-    a=Decimal(input("ainput:"))
-    b=Decimal(input("binput:"))
-    c=Decimal(input("cinput:"))
+def quadratic(a,b,c):
+    a=Decimal(a)
+    b=Decimal(b)
+    c=Decimal(c)
     x_1=(-b+(b**2-4*a*c)**Decimal(0.5))/(2*a)
     x_2=(-b-(b**2-4*a*c)**Decimal(0.5))/(2*a)
     x_3=2*c/(-b+(b**2-4*a*c)**Decimal(0.5))
     x_4=2*c/(-b-(b**2-4*a*c)**Decimal(0.5))
     print(x_1,x_2,x_3,x_4)
-    return 0
+    return x_1,x_2
