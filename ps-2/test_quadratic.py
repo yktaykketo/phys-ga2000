6b76150d144653c57eb2bbd8e46d247dcddedbb6
yktaykketo <102287488+yktaykketo@users.noreply.github.com>
2023-09-25 17:27:26 -0400

    Update test_quadratic.py

--- ps-2/test_quadratic.py
+++ ps-2/test_quadratic.py
@@ -6,19 +6,24 @@ Created on Tue Sep 19 18:23:50 2023
 @author: wangziyao
 """
 
-import numpy as np
 import quadratic
+from decimal import Decimal, getcontext
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams['text.usetex'] = True
 def test_quadratic():
       # Check the case from the problem
-      x1, x2 = quadratic.quadratic(a=0.001, b=1000., c=0.001)
-      assert (np.abs(x1 - (- 1.e-6)) < 1.e-10)
-      assert (np.abs(x2 - (- 0.999999999999e+6)) < 1.e-10)
+      x1, x2 = quadratic.quadratic(0.001, 1000., 0.001)
+      assert (np.abs(x1 - Decimal(- 1.e-6)) < 1.e-10)
+      assert (np.abs(x2 - Decimal(- 0.999999999999e+6)) < 1.e-10)
       # Check a related case to the problem
-      x1, x2 = quadratic.quadratic(a=0.001, b=-1000., c=0.001)
-      assert (np.abs(x1 - (0.999999999999e+6)) < 1.e-10)
-      assert (np.abs(x2 - (1.e-6)) < 1.e-10)
+      x1, x2 = quadratic.quadratic(0.001, -1000., 0.001)
+      assert (np.abs(x1 - Decimal(0.999999999999e+6)) < 1.e-10)
+      assert (np.abs(x2 - Decimal(1.e-6)) < 1.e-10)
       # Check a simpler case (note it requires the + solution first)
-      x1, x2 = quadratic.quadratic(a=1., b=8., c=12.)
-      assert (np.abs(x1 - (- 2.)) < 1.e-10)
-      assert (np.abs(x2 - (- 6)) < 1.e-10)
+      x1, x2 = quadratic.quadratic(1., 8., 12.)
+      assert (np.abs(x1 - Decimal(- 2.)) < 1.e-10)
+      assert (np.abs(x2 - Decimal(- 6)) < 1.e-10)
+     
      
